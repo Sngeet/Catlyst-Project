67a5d1c1c57a2abc29c4c0b7f519e250d0ef3d37
Sangeeth746 <sangeethmohan746@gmail.com>
2026-09-09 10:16:58 +0530

    Added error handling Python file

---
+++ errorhandling.py
@@ -0,0 +1,115 @@
+# try:
+#     student={"name":"shin","course":"python"}
+#     print(student["name"])
+# except KeyError:
+#     print("Key not found")
+# else:
+#     print("Key found")
+
+
+# try:
+#     a=10
+#     b=20
+# except TypeError:
+#     print("Type Error")
+# else:
+#     print(a+b)
+# finally:
+#     print("Code completed")
+
+# try:
+#     lst=[10,20,30]
+#     print(lst[5])
+# except IndexError:
+#     print("index error occured")
+# else:
+#     print(lst)
+# finally:
+#     print("Done")
+
+
+#Questionss
+
+
+# 14.Create a list containing 5 numbers and find:
+# Length
+# Maximum
+# Minimum
+# Sum
+
+# 15.Create a tuple containing 5 fruits and access the third fruit
+
+# 16.Create a dictionary containing:
+# name
+# age
+# course
+# and access each value.
+# Take a list of numbers and remove duplicate values
+# Take two lists and find their common elements using a set
+# keys = ["name", "age", "course"]
+# values = ["Anu", 25, "Python"]
+
+# 17.Find the second-largest number in a list
+# Check datatype
+# a = 10
+# b = 10.5
+# c = "Python"
+# d = True
+# e = [1, 2, 3]
+# f = (1, 2, 3)
+# g = {1, 2, 3}
+# h = {"name": "Anu"}
+
+# 18.Create a list of 5 numbers. Print the first element, last element, and length of the list
+# fruits = ["apple", "banana", "orange"]
+# Change "banana" to "mango".
+
+# 19.Create a tuple containing 5 colors. Print the second and fourth colors
+
+# a = {1, 2, 3, 4}
+# b = {3, 4, 5, 6}
+# Find:
+# Union
+# Intersection
+# Difference
+
+# 20.Add "place": "Kerala" to a dictionary
+
+# 21.Find the largest and smallest number:
+# numbers = [45, 12, 78, 23, 9, 56]
+
+# 22.Separate Even and Odd
+# Given:
+# numbers = [10, 15, 22, 33, 40, 51]
+
+# 23.Create two lists:
+# even
+# odd
+
+# 24.Find the common elements between:
+# a = [1, 2, 3, 4, 5]
+# b = [4, 5, 6, 7, 8]
+
+# 25.Create a Dictionary from Two Lists
+# keys = ["name", "age", "course"]
+# values = ["Anu", 25, "Python"]
+
+# 26.Count how many times each character occurs:
+# text = "banana"
+# Expected result:
+# {'b': 1, 'a': 3, 'n': 2}
+
+# 27.Given:
+# students = {
+#     "Anu": 85,
+#     "Rahul": 72,
+#     "Meera": 91,
+#     "Arun": 65
+# }
+# Print the names of students who scored more than 80
+
+# 30.Take an integer and convert into string
+
+# 31.Take input from user and print it's sum
+
+# 32.Store a integer value as string from user and find it's sum
